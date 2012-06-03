c2b1ed541d657c198e11b8b38323b280a53d2206
Sergey Todyshev <sergey.todyshev@grapecity.com>
2012-06-03 15:39:29 +0700

    added test for conditional rethrow bug

---
+++ source/tests/DataDynamics.PageFX.QA/TestCases/PageFX/Simple/CSharp/TryCatch/ConditionalRethrow.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+internal class Test
+{
+    bool _running;
+
+    void Test1()
+    {
+        Console.WriteLine("Test::Foo");
+        throw new Exception("aaa");
+    }
+
+	public class TryCatchStatement
+	{
+		public object Catch { get; set; }
+	}
+
+    public object Run(TryCatchStatement statement)
+    {
+        try
+        {
+            _running = true;
+            Test1();
+            return this;
+        }
+        catch (Exception e)
+        {
+			if (statement.Catch != null)
+			{
+				Console.WriteLine(statement.Catch);
+			}
+			else
+			{
+				throw;
+			}
+        }
+        finally
+        {
+            _running = false;
+        }
+    	return null;
+    }
+    
+    static void Main()
+    {
+        var test = new Test();
+        try
+        {
+            test.Run(new TryCatchStatement
+                     	{
+                     		Catch = "catch me"
+                     	});
+        	test.Run(new TryCatchStatement());
+		}
+        catch (Exception e)
+        {
+            Console.WriteLine("exception: {0}", e.GetType());
+        }
+        Console.WriteLine("<%END%>");
+    }
+}
---
+++ source/tests/DataDynamics.PageFX.QA/TestCases/PageFX/Simple/CSharp/TryCatch/EncodingDefault.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+internal class Test
+{
+    public object Run()
+    {
+    	var e = Encoding.Default;
+    	return e;
+    }
+    
+    static void Main()
+    {
+        var test = new Test();
+        try
+        {
+            test.Run();
+		}
+        catch (Exception e)
+        {
+            Console.WriteLine("exception: {0}", e.GetType());
+        }
+        Console.WriteLine("<%END%>");
+    }
+}
