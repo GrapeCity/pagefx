6da73b04af9f8b384bd672407b244e3ff4e2de51
Sergey Todyshev <sergey.todyshev@grapecity.com>
2012-08-14 13:51:02 +0700

    array tests

---
+++ source/tests/DataDynamics.PageFX.QA/TestCases/PageFX/Simple/CSharp/Arrays/FirstTest.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Test
+{
+    public static void Main()
+    {
+		int[] arr = new int[3];
+		arr[0] = 10;
+		arr[1] = 20;
+		arr[2] = 30;
+		Console.WriteLine(arr[0]);
+		Console.WriteLine(arr[1]);
+		Console.WriteLine(arr[2]);
+        Console.WriteLine("<%END%>");
+    }
+}
--- source/tests/DataDynamics.PageFX.QA/TestCases/PageFX/Simple/CSharp/Arrays/SmallTests.cs
+++ source/tests/DataDynamics.PageFX.QA/TestCases/PageFX/Simple/CSharp/Arrays/SmallTests.cs
@@ -10,14 +10,20 @@ class X
 {
     static void Test1()
     {
+	    Console.WriteLine("Test1");
         int[] arr = new int[] { 10, 20, 30 };
         Console.WriteLine(arr[0]);
         Console.WriteLine(arr[1]);
         Console.WriteLine(arr[2]);
+		arr = new int[] { -10, -20, -30 };
+		Console.WriteLine(arr[0]);
+		Console.WriteLine(arr[1]);
+		Console.WriteLine(arr[2]);
     }
 
     static void Test2()
     {
+		Console.WriteLine("Test2");
         byte[] arr = new byte[] { 0x00, 0x01, 0x02, 0x03 };
         Console.WriteLine(arr.Length);
         Console.WriteLine(arr[0]);
@@ -28,6 +34,7 @@ class X
 
     static void Test3()
     {
+		Console.WriteLine("Test3");
         string[] arr = new string[] { "aaa", "bbb", "ccc" };
         Console.WriteLine(arr[0]);
         Console.WriteLine(arr[1]);
@@ -36,6 +43,7 @@ class X
 
     static void Test4()
     {
+		Console.WriteLine("Test4");
         Point[] arr = new Point[3];
         arr[0].x = 1;
         arr[0].y = 1;
@@ -53,6 +61,7 @@ class X
 
     static void Test5()
     {
+		Console.WriteLine("Test5");
         int n = 10;
         Point[] arr = new Point[n];
         for (int i = 0; i < n; ++i)
@@ -69,6 +78,7 @@ class X
 
     static void Test6()
     {
+		Console.WriteLine("Test6");
         int inputCount = 6;
         int blockLen = 3;
         int outLen = 4;
@@ -80,6 +90,7 @@ class X
 
     static void Test7()
     {
+		Console.WriteLine("Test7");
         double[] arr = new double[] { 10, 20, 30 };
         for (int i = 0; i < arr.Length; ++i)
             Console.WriteLine(arr[i]);
@@ -87,6 +98,7 @@ class X
 
     static void Test8()
     {
+		Console.WriteLine("Test8");
         byte[] buf = new byte[] { 65, 1, 32, 43, 5, 3, 54, 0 };
         int v = (buf[0] | (buf[1] << 8) | (buf[2] << 16) | (buf[3] << 24));
         Console.WriteLine(v);
@@ -94,6 +106,7 @@ class X
 
     static void Test9()
     {
+		Console.WriteLine("Test9");
         {
             byte b = 32;
             int v = b << 16;
@@ -108,8 +121,6 @@ class X
 
     public static void Main()
     {
-        Console.WriteLine("Hello!");
-        Console.WriteLine(100);
         Test1();
         Test2();
         Test3();
@@ -117,8 +128,6 @@ class X
         Test5();
         Test6();
         Test7();
-        Console.WriteLine("--- Test8");
-        Console.WriteLine(723517761);
         Test8();
         Test9();
         Console.WriteLine("<%END%>");
