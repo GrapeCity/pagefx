ef3dcdadd7b8f0d01ee74b6bf242f0c9f0a3f95d
Sergey Todyshev <sergey.todyshev@grapecity.com>
2012-11-13 17:26:53 +0700

    tuples

--- source/libs/DataDynamics.PageFX.Shared/System.Collections.Generic/Tuples.cs
+++ source/libs/DataDynamics.PageFX.Shared/System.Collections.Generic/Tuples.cs
@@ -248,6 +248,29 @@
 		}
 	}
 
+	public static class Tuple
+	{
+		public static Tuple<T> Create<T>(T value)
+		{
+			return new Tuple<T>(value);
+		}
+
+		public static Tuple<T1,T2> Create<T1,T2>(T1 item1, T2 item2)
+		{
+			return new Tuple<T1,T2>(item1, item2);
+		}
+
+		public static Tuple<T1, T2, T3> Create<T1, T2, T3>(T1 item1, T2 item2, T3 item3)
+		{
+			return new Tuple<T1, T2, T3>(item1, item2, item3);
+		}
+
+		public static Tuple<T1, T2, T3, T4> Create<T1, T2, T3, T4>(T1 item1, T2 item2, T3 item3, T4 item4)
+		{
+			return new Tuple<T1, T2, T3, T4>(item1, item2, item3, item4);
+		}
+	}
+
 	public sealed class Tuple<T> :
 		ITuple<T>, IComparable,
 		IComparable<Tuple<T>>, IEquatable<Tuple<T>>,
