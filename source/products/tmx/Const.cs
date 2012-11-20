aa33bdfddc7c34c3e631279aab50917b1f104566
Sergey Todyshev <sergey.todyshev@grapecity.com>
2012-11-20 21:21:30 +0700

    cleanup

--- source/products/tmx/Const.cs
+++
@@ -1,9 +0,0 @@
-namespace tmx
-{
-    class Const
-    {
-        public const string DefaultNamespace = "DataDynamics";
-
-        public const string MxNamespaceUri = "http://www.adobe.com/2006/mxml";
-    }
-}
