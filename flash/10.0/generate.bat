0020e4a85551d424300ee31b038b326aa0f91358
Sergey Todyshev <sergey.todyshev@grapecity.com>
2013-01-26 21:10:10 +0700

    updated scripts to generate api for flash player v10.0

--- flash/10.0/generate.bat
+++ flash/10.0/generate.bat
@@ -1,4 +1,1 @@
-pfc /wrap /dir:FP10 /FP:10 fp10.abc
-del fp10.dll;fp10.pdb
-del .\FP10\abc.cs
-del .\FP10\Avm\Vector.cs
+pfc /wrap /nocsc /dir:. /FP:10 flash.abc
---
+++ flash/10.0/swc2abc.bat
@@ -0,0 +1,1 @@
+abc /merge flash.swc
